ecc8992c8b51ba19b7519e9cb2bb33777456a129
crucials <internetuser553@gmail.com>
2023-03-27 13:44:27 +0500

    Changed interval time to 10 minutes

--- server/plugins/api-wake-up.ts
+++ server/plugins/api-wake-up.ts
@@ -3,5 +3,5 @@ import { API_BASE_URL } from '~~/api'
 export default defineNitroPlugin((nitroApp) => {
     setInterval(async () => {
         await $fetch(API_BASE_URL + '/pictures')
-    }, 13800 * 60)
+    }, 10000 * 60)
 })
