03fd0ba2efbbb653972f7e6b37bd6be5cd3e49c4
crucials <internetuser553@gmail.com>
2023-03-27 22:05:17 +0500

    Renamed export property to `handler`

--- netlify/functions/api-wake-up.ts
+++ netlify/functions/api-wake-up.ts
@@ -2,7 +2,7 @@ import { Handler, HandlerEvent, HandlerContext, schedule } from '@netlify/functi
 import fetch from 'node-fetch'
 import { API_BASE_URL } from '~~/api'
 
-const handler : Handler = async (event: HandlerEvent, context: HandlerContext) => {
+const scheduledHandler : Handler = async (event: HandlerEvent, context: HandlerContext) => {
    await fetch(API_BASE_URL + '/pictures')
 
    return {
@@ -10,6 +10,6 @@ const handler : Handler = async (event: HandlerEvent, context: HandlerContext) =
    }
 }
 
-const scheduledHandler = schedule('*/10 * * * *', handler)
+const handler = schedule('*/10 * * * *', scheduledHandler)
 
-export { scheduledHandler }
+export { handler }
