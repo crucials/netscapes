390a8cc4aacd0fa26cf4080ac960627cd704aebd
crucials <internetuser553@gmail.com>
2023-03-27 22:02:28 +0500

    Add response in return

--- netlify/functions/api-wake-up.ts
+++ netlify/functions/api-wake-up.ts
@@ -1,9 +1,13 @@
-import { Handler, HandlerEvent, schedule } from '@netlify/functions'
+import { Handler, HandlerEvent, HandlerContext, schedule } from '@netlify/functions'
 import fetch from 'node-fetch'
 import { API_BASE_URL } from '~~/api'
 
-const handler : Handler = () => {
-   fetch(API_BASE_URL + '/pictures')
+const handler : Handler = async (event: HandlerEvent, context: HandlerContext) => {
+   await fetch(API_BASE_URL + '/pictures')
+
+   return {
+      statusCode: 200,
+   }
 }
 
 const scheduledHandler = schedule('*/10 * * * *', handler)
