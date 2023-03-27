67f09a115f569e7e8056af5487cfcf39ad2c80b0
crucials <internetuser553@gmail.com>
2023-03-27 16:49:21 +0500

    Deleted `server` folder

--- server/plugins/api-wake-up.ts
+++
@@ -1,7 +0,0 @@
-import { API_BASE_URL } from '~~/api'
-
-export default defineNitroPlugin((nitroApp) => {
-    setInterval(async () => {
-        await $fetch(API_BASE_URL + '/pictures')
-    }, 1000 * 60)
-})
